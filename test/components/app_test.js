4d9b6f950138d73937bcb34b0462e4aa429e9cf0
Maggie Serino <m.serino@hotmail.it>
2018-05-10 16:46:50 +0200

    App shows a CommentBox

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -2,10 +2,13 @@ import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
 describe('App', () => {
-    it('shows the correct text', () => {
-        // create an instance of App
-        const component = renderComponent(App);
-        
-        expect(component).to.contain('React simple starter');
+    let component;
+
+    beforeEach(() => {
+        component = renderComponent(App);
+    });
+
+    it('shows a comment box', () => {
+        expect(component.find('.comment-box')).to.exist;
     });
 });
