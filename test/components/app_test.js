01f8e53569b67e131c2e30829d2169d365ef1662
Maggie Serino <m.serino@hotmail.it>
2018-05-10 16:36:55 +0200

    CommentBox has button and textarea

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,11 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
-
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
-
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
+describe('App', () => {
+    it('shows the correct text', () => {
+        // create an instance of App
+        const component = renderComponent(App);
+        
+        expect(component).to.contain('React simple starter');
+    });
 });
